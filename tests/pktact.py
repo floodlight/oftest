b0b0fdbe3cb18587902d79e98ba0ab6df03941f6
Dan Talayco <dtalayco@stanford.edu>
2010-05-11 15:44:56 -0700

    Added DirectMCNonIngress test case
    Tests multicast to all non-ingress ports

--- tests/pktact.py
+++ tests/pktact.py
@@ -154,7 +154,7 @@ class DirectTwoPorts(basic.SimpleDataPlane):
             self.assertTrue(request.actions.add(act), "Could not add action1")
             act.port = egress_port2
             self.assertTrue(request.actions.add(act), "Could not add action2")
-            pa_logger.info(request.show())
+            # pa_logger.info(request.show())
 
             pa_logger.info("Inserting flow")
             rv = self.controller.message_send(request)
@@ -185,5 +185,77 @@ class DirectTwoPorts(basic.SimpleDataPlane):
                              'Response packet does not match send packet 1')
             self.assertEqual(str(pkt), str(rcv_pkt2),
                              'Response packet does not match send packet 2')
+            
+        
+
+class DirectMCNonIngress(basic.SimpleDataPlane):
+    """
+    Multicast to all non-ingress ports
+
+    Generate a packet
+    Generate and install a matching flow
+    Add action to direct the packet to all non-ingress ports
+    Send the packet to ingress dataplane port
+    Verify the packet is received at all non-ingress ports
+
+    Does not use the flood action
+    """
+    def runTest(self):
+        global pa_port_map
+        of_ports = pa_port_map.keys()
+        of_ports.sort()
+        self.assertTrue(len(of_ports) > 2, "Not enough ports for test")
+
+        rc = delete_all_flows(self.controller, pa_logger)
+        self.assertEqual(rc, 0, "Failed to delete all flows")
+
+        pkt = simple_tcp_packet()
+        match = parse.packet_to_flow_match(pkt)
+        match.wildcards &= ~ofp.OFPFW_IN_PORT
+        self.assertTrue(match is not None, 
+                        "Could not generate flow match from pkt")
+        act = action.action_output()
+
+        for idx in range(len(of_ports)):
+            ingress_port = of_ports[idx]
+            pa_logger.info("Ingress " + str(ingress_port) + 
+                           " all non-ingress ports")
+
+            match.in_port = ingress_port
+
+            request = message.flow_mod()
+            request.match = match
+            request.buffer_id = 0xffffffff
+            for egr_idx in range(len(of_ports)):
+                if egr_idx == idx:
+                    continue
+                egress_port = of_ports[egr_idx]
+                act.port = egress_port
+                self.assertTrue(request.actions.add(act), 
+                                "Could not add output to " + str(egress_port))
+            pa_logger.info(request.show())
+
+            pa_logger.info("Inserting flow")
+            rv = self.controller.message_send(request)
+            self.assertTrue(rv != -1, "Error installing flow mod")
+            do_barrier(self.controller)
+
+            pa_logger.info("Sending packet to dp port " + 
+                           str(ingress_port))
+            self.dataplane.send(ingress_port, str(pkt))
+            for egr_idx in range(len(of_ports)):
+                if egr_idx == idx:
+                    continue
+                ofport = of_ports[egr_idx]
+                (rcv_port, rcv_pkt, pkt_time) = self.dataplane.poll(
+                    port_number=ofport, timeout=1)
+                self.assertTrue(rcv_pkt is not None, 
+                                "Did not receive packet port " + str(ofport))
+                pa_logger.debug("Packet len " + str(len(rcv_pkt)) + " in on "
+                                + str(rcv_port))
+
+                self.assertEqual(str(pkt), str(rcv_pkt),
+                             'Response packet does not match send packet ' +
+                                 "on port " + str(ofport))
             
         
