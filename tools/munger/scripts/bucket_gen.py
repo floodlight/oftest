001c86239268b98dbb0bffc54c9dfb675ce6a442
Dan Talayco <dtalayco@stanford.edu>
2010-11-24 01:35:26 -0800

    Fix order of decs in init

--- tools/munger/scripts/bucket_gen.py
+++ tools/munger/scripts/bucket_gen.py
@@ -5,15 +5,14 @@
 
 import sys
 sys.path.append("../../src/python/oftest")
-from cstruct import *
 from class_maps import class_to_members_map
 from common_gen import *
 
 print """
 # Python OpenFlow bucket wrapper class
 
-from cstruct import *
-from action_list import action_list
+from oftest.cstruct import ofp_bucket
+from oftest.action_list import action_list
 
 """
 
@@ -26,8 +25,8 @@ class bucket(--PARENT_TYPE--):
     \"""
     def __init__(self):
         --PARENT_TYPE--.__init__(self)
-        self.len = self.__len__()
         self.actions = action_list()
+        self.len = self.__len__()
     def show(self, prefix=''):
         outstr = prefix + "bucket\\n"
         outstr += --PARENT_TYPE--.show(self, prefix)
