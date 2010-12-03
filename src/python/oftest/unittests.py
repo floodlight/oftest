360a7cb815d5b539861ae3d01b7fcce355e825c7
Rob Sherwood <rob.sherwood@stanford.edu>
2010-12-02 23:34:58 -0800

    added unittests for oftests/{messages,instruction,instruciton_list}
    very basic, was trying to find a bug

---
+++ src/python/oftest/unittests.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import unittest
+from message_unittests import *
+from instruction import *
+from instruction_list import *
+from packet import *
+
+if __name__ == '__main__':
+    unittest.main()
