e23e856a8bb16b7dabc24bef4c0c819cb970c4b4
Rob Sherwood <rob.sherwood@stanford.edu>
2010-12-07 11:51:47 -0800

    updated ofps_regression.sh with all working tests
    included a master list of what passes, fails, and has errors

--- tests/ofps_regression.sh
+++ tests/ofps_regression.sh
@@ -1,6 +1,55 @@
 #!/bin/sh
 echo Assumes ofps is running somewhere else
 echo    like via run_switch.py
-exec ./oft --verbose --test-spec=Echo,EchoWithData,FlowRemoveAll,PacketIn,PacketOut,\
-FlowStatsGet,FlowStats,DirectPacket,DirectPacketICMP,TwoTable1
+exec ./oft --verbose --test-spec=\
+Echo,EchoWithData,FlowRemoveAll,PacketIn,PacketOut,\
+FlowStatsGet,FlowStats,DirectPacket,DirectPacketICMP,TwoTable1,\
+BaseMatchCase
 
+##### output from `./oft --verbose 2>&1 | grep runTest | sort -r -k 4`
+# runTest (pktact.TwoTable1) ... ok
+# runTest (pktact.DirectPacket) ... ok
+# runTest (pktact.DirectPacketICMP) ... ok
+# runTest (pktact.BaseMatchCase) ... ok
+# runTest (flow_stats.FlowStats) ... ok
+# runTest (basic.SimpleProtocol) ... ok
+# runTest (basic.SimpleDataPlane) ... ok
+# runTest (basic.PacketOut) ... ok
+# runTest (basic.PacketIn) ... ok
+# runTest (basic.FlowStatsGet) ... ok
+# runTest (basic.FlowRemoveAll) ... ok
+# runTest (basic.FlowMod) ... ok
+# runTest (basic.FeaturesRequest) ... ok
+# runTest (basic.EchoWithData) ... ok
+# runTest (basic.Echo) ... ok
+# runTest (basic.DataPlaneOnly) ... ok
+# runTest (pktact.StripVLANTag) ... FAIL
+# runTest (pktact.ModifyVID) ... FAIL
+# runTest (pktact.ModifyTOS) ... FAIL
+# runTest (pktact.ModifyL4Src) ... FAIL
+# runTest (pktact.ModifyL4Dst) ... FAIL
+# runTest (pktact.ModifyL3Src) ... FAIL
+# runTest (pktact.ModifyL3Dst) ... FAIL
+# runTest (pktact.ModifyL2Src) ... FAIL
+# runTest (pktact.ModifyL2Dst) ... FAIL
+# runTest (pktact.AddVLANTag) ... FAIL
+# runTest (basic.TableStatsGet) ... FAIL
+# runTest (pktact.SingleWildcardMatchTagged) ... ERROR
+# runTest (pktact.SingleWildcardMatch) ... ERROR
+# runTest (pktact.FloodPlusIngress) ... ERROR
+# runTest (pktact.FloodMinusPort) ... ERROR
+# runTest (pktact.Flood) ... ERROR
+# runTest (pktact.ExactMatchTagged) ... ERROR
+# runTest (pktact.ExactMatch) ... ERROR
+# runTest (pktact.DirectTwoPorts) ... ERROR
+# runTest (pktact.DirectMCNonIngress) ... ERROR
+# runTest (pktact.DirectMC) ... ERROR
+# runTest (pktact.AllWildcardMatchTagged) ... ERROR
+# runTest (pktact.AllWildcardMatch) ... ERROR
+# runTest (pktact.AllPlusIngress) ... ERROR
+# runTest (pktact.AllExceptOneWildcardMatchTagged) ... ERROR
+# runTest (pktact.AllExceptOneWildcardMatch) ... ERROR
+# runTest (pktact.All) ... ERROR
+# runTest (flow_expire.FlowExpire) ... ERROR
+# runTest (caps.FillTableWC) ... ERROR
+# runTest (basic.PortConfigMod) ... ERROR
