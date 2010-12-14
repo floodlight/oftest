b06af816075ce6a7115dca1dee38889c109fee49
Rob Sherwood <rob.sherwood@stanford.edu>
2010-12-13 19:39:01 -0800

    Updated flow_expire test to of-1.1
    * ofps still fails, b/c flow_expiration is not really implemented

--- tests/flow_expire.py
+++ tests/flow_expire.py
@@ -19,6 +19,7 @@ import basic
 
 from testutils import *
 from time import sleep
+import testutils
 
 #@var port_map Local copy of the configuration map from OF port
 # numbers to OS interfaces
@@ -63,34 +64,35 @@ class FlowExpire(basic.SimpleDataPlane):
         self.assertEqual(rc, 0, "Failed to delete all flows")
 
         pkt = simple_tcp_packet()
-        match = parse.packet_to_flow_match(pkt)
-        match.wildcards &= ~ofp.OFPFW_IN_PORT
-        self.assertTrue(match is not None, 
-                        "Could not generate flow match from pkt")
-        act = action.action_output()
-
+#        match = parse.packet_to_flow_match(pkt)
+#        match.wildcards &= ~ofp.OFPFW_IN_PORT
+#        self.assertTrue(match is not None, 
+#                        "Could not generate flow match from pkt")
+#        act = action.action_set_output_port()
+#
         ingress_port = pa_config["base_of_port"]
         egress_port  = (pa_config["base_of_port"] + 1) % len(of_ports)
         pa_logger.info("Ingress " + str(ingress_port) + 
                        " to egress " + str(egress_port))
-        
-        match.in_port = ingress_port
-        
-        request = message.flow_mod()
-        request.match = match
+#        
+#        match.in_port = ingress_port
+#        
+#        request = message.flow_mod()
+#        request.match = match
+#        
+        request = testutils.flow_msg_create(self, pkt, ingress_port, egr_port=egress_port, check_expire=True)
         request.cookie = random.randint(0,9007199254740992)
         request.buffer_id = 0xffffffff
         request.idle_timeout = 1
         request.flags |= ofp.OFPFF_SEND_FLOW_REM
-        act.port = egress_port
-        self.assertTrue(request.actions.add(act), "Could not add action")
+        match = request.match
         
         pa_logger.info("Inserting flow")
         rv = self.controller.message_send(request)
         self.assertTrue(rv != -1, "Error installing flow mod")
         do_barrier(self.controller)
 
-        (response, raw) = self.controller.poll(ofp.OFPT_FLOW_REMOVED, 2)
+        (response, _) = self.controller.poll(ofp.OFPT_FLOW_REMOVED, 2)
         
         self.assertTrue(response is not None, 
                         'Did not receive flow removed message ')
