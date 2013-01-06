95f078b7d0dcc168c9e92f037812d3b3075ef0f8
Rich Lane <rich.lane@bigswitch.com>
2013-01-06 13:24:58 -0800

    oft: improve help text

---
+++ src/python/oftest/help_formatter.py
@@ -0,0 +1,10 @@
+import optparse
+
+# Don't wrap description text to give us more control over newlines.
+class HelpFormatter(optparse.IndentedHelpFormatter):
+    def format_description(self, description):
+        if description:
+            indent = " "*self.current_indent
+            return indent + description
+        else:
+            return none
