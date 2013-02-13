561f7c5a2ff89af0d969080d11b789b9baa36e3f
ShreyaPandita <sdnadmin@SDNLABCOMPUTER.ads.iu.edu>
2013-02-13 12:00:26 -0500

    edited

--- tests/FuncUtils.py
+++ tests/FuncUtils.py
@@ -30,7 +30,7 @@ def match_ip_src(self,of_ports,wildcard_bits,priority=None):
     # @param val is number of bits we need to wild-card in the ip_src add
     # @ can take values from 0 (exact-match) 32 (for wild-card all)
     val = wildcard_bits
-    wildcards = (ofp.OFPFW_ALL & ~ofp.OFPFW_NW_SRC_MASK) | (val << ofp.OFPFW_NW_SRC_SHIFT)
+    wildcards = (ofp.OFPFW_ALL & ofp.OFPFW_NW_SRC_MASK) | (val << ofp.OFPFW_NW_SRC_SHIFT)
     msg = message.flow_mod()
     msg.match = match
     if priority != None :
