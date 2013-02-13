9ab33cb54da3b293bb4920a87c8e68c8a8c87fa1
ShreyaPandita <sdnadmin@SDNLABCOMPUTER.ads.iu.edu>
2013-02-13 10:06:06 -0500

    edited

--- tests/testgroup50.py
+++ tests/testgroup50.py
@@ -228,6 +228,8 @@ class Grp50No50(base_tests.SimpleDataPlane):
         logging.info("Sending matching and non-matching ethernet packets")
         logging.info("Verifying only matching packets implements the action specified in the flow")
 
+        sleep(5)        
+
         #Insert a Match on Ethernet-Type flow
         (pkt,match) = match_ethernet_type(self,of_ports)   
 
