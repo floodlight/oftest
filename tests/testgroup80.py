b81bd6176afd3520863a209cb8dde674bc246e02
ShreyaPandita <sdnadmin@SDNLABCOMPUTER.ads.iu.edu>
2013-02-19 16:15:55 -0500

    edited

--- tests/testgroup80.py
+++ tests/testgroup80.py
@@ -98,7 +98,7 @@ class Grp80No30(base_tests.SimpleProtocol):
                                'Switch did not reply with error message')
         self.assertTrue(response.type==ofp.OFPET_BAD_REQUEST, 
                                'Message field type is not OFPET_BAD_REQUEST') 
-        self.assertTrue(response.type==ofp.OFPET_BAD_REQUEST, 
+        self.assertTrue(response.code==ofp.OFPBRC_BAD_VERSION, 
                                'Message field code is not OFPBRC_BAD_VERSION')
 
 
