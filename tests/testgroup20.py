e8e1f25bb05a84834edc2325333217e25e4277bb
ShreyaPandita <sdnadmin@SDNLABCOMPUTER.ads.iu.edu>
2013-03-08 12:06:24 -0500

    edited

--- tests/testgroup20.py
+++ tests/testgroup20.py
@@ -309,7 +309,7 @@ class Grp20No90(base_tests.SimpleDataPlane):
 
         of_ports = config["port_map"].keys()
         of_ports.sort()
-        ingress_port = of_ports[2]
+        ingress_port = of_ports[3]
 
         #Clear Switch state
         rc = delete_all_flows(self.controller)
