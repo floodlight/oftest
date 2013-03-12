c7b85d2cdfade2a5b9af25fe4283a18a4de7e811
Rich Lane <rlane@bigswitch.com>
2013-03-11 17:55:20 -0700

    loxi-prep: remove import compatibility

--- src/python/oftest/__init__.py
+++ src/python/oftest/__init__.py
@@ -8,9 +8,3 @@ config = {}
 # Global DataPlane instance used by all tests.
 # Populated by oft.
 dataplane_instance = None
-
-# Alias of10 modules into oftest namespace for backwards compatbility
-import of10
-from of10 import *
-for modname in of10.__all__:
-    sys.modules["oftest." + modname] = sys.modules["of10." + modname]
