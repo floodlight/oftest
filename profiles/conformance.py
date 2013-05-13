11d4169c69af4c91f6f7a68aebb0da3f22eafafd
Jonathan Stout <jonstout@indiana.edu>
2013-05-13 17:04:25 -0400

    added match types for l2, l3, and full profiles

--- profiles/conformance.py
+++ profiles/conformance.py
@@ -1,15 +1,15 @@
 profiles = {
     'l2' : {
         'mandatory' : 'Grp50No10,Grp50No20,Grp50No30,Grp50No40,Grp50No50,Grp50No60,Grp50No140,Grp50No190',
-        'matches' : 'm1,m2,m3'
+        'matches' : 'dl_dst,dl_src,dl_type,dl_vlan,dl_vlan_pcp'
         },
     'l3' : {
         'mandatory' : 'Grp50No10,Grp50No20,Grp50No50,Grp50No80,Grp50No90,Grp50No150,Grp50No180,Grp50No190',
-        'matches' : 'm4,m5,m6'
+        'matches' : 'nw_src,nw_dst'
         },
     'full' : {
         'mandatory' : 'Grp50No10,Grp50No20,Grp50No30,Grp50No40,Grp50No50,Grp50No60,Grp50No80,Grp50No90,Grp50No140,Grp50No150,Grp50No180,Grp50No190',
-        'matches' : 'm1,m2,m3,m4,m5,m6'
+        'matches' : 'dl_dst,dl_src,dl_type,dl_vlan,dl_vlan_pcp,nw_src,nw_dst'
         },
     # Add custom match profiles below
 }
