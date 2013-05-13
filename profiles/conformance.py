bf2333365068e95ae62972e25796722ff61b852f
Jonathan Stout <jonstout@indiana.edu>
2013-05-13 12:58:59 -0400

    marking tests as mandatory if defined in profile

---
+++ profiles/conformance.py
@@ -0,0 +1,15 @@
+profiles = {
+    'l2' : {
+        'mandatory' : 'Grp50No10,Grp50No20,Grp50No30,Grp50No40,Grp50No50,Grp50No60,Grp50No140,Grp50No190',
+        'matches' : 'm1,m2,m3'
+        },
+    'l3' : {
+        'mandatory' : 'Grp50No10,Grp50No20,Grp50No50,Grp50No80,Grp50No90,Grp50No150,Grp50No180,Grp50No190',
+        'matches' : 'm4,m5,m6'
+        },
+    'full' : {
+        'mandatory' : 'Grp50No10,Grp50No20,Grp50No30,Grp50No40,Grp50No50,Grp50No60,Grp50No80,Grp50No90,Grp50No140,Grp50No150,Grp50No180,Grp50No190',
+        'matches' : 'm1,m2,m3,m4,m5,m6'
+        },
+    # Add custom match profiles below
+}
