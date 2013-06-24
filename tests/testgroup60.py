492929e753a771cb78c946db75bf762c49a3bf02
Jonathan Stout <jonstout@indiana.edu>
2013-06-24 11:27:26 -0400

    adding check for unsupported counter

--- tests/testgroup60.py
+++ tests/testgroup60.py
@@ -189,6 +189,12 @@ class Grp60No40(base_tests.SimpleDataPlane):
             if duration[1] < previous_duration[1]:
                 self.assertGreater(duration[0], previous_duration[0], "Duration in nsecs was less than previous duration in nsecs, but the duration in secs was not greater than the previous duration in secs.")
             else:
+                # In the case that a switch can't report in
+                # millisecond granularity, we expect
+                # duration_nsec to be reported as 0xffffffff.
+                if duration[1] == previous_duration[1] and duration[1] == 4294967295:
+                    logging.info("Duration in nsec is not supported. Reported as -1 or 0xffffffff")
+                    return
                 self.assertNotEqual(duration[1], previous_duration[1], "ofp_flow_stats.duration_nsec {0} was the same as the previous duration_nsec {1}.".format(duration, previous_duration))
             previous_duration = duration
             time.sleep(1.5)
