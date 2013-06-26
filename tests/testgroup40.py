353e6f34d7741f18d115819821d5cc52f029a9bb
sumithdev09 <sumith.hy398@gmail.com>
2013-06-26 15:43:18 +0000

    fixed bitwise OR to logical OR

--- tests/testgroup40.py
+++ tests/testgroup40.py
@@ -287,12 +287,8 @@ class Grp40No50(base_tests.SimpleProtocol):
                                                timeout=5)
             if not response:  # Timeout
                 break
-            if not response.type == ofp.OFPET_BAD_ACTION | ofp.OFPET_FLOW_MOD_FAILED:
-                logging.info("Error type is not as expected")
-                break
-            if not response.code == ofp.OFPPMFC_BAD_PORT | ofp.OFPFMFC_EPERM:
-                logging.info("Error field code is not as expected")
-                break
+            self.assertTrue(response.type == ofp.OFPET_BAD_ACTION or response.type == ofp.OFPET_FLOW_MOD_FAILED, "Error type is not as expected")
+            self.assertTrue(response.code == ofp.OFPBAC_BAD_OUT_PORT or response.code == ofp.OFPFMFC_EPERM, "Error field code is not as expected")
             if not config["relax"]:  # Only one attempt to match
                 break
             count += 1
