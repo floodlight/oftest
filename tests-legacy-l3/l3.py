e1837066e3cd4fa5c45c2b8783d5730c0ad4992c
Rob Sherwood <rob.sherwood@bigswitch.com>
2015-11-12 18:04:29 -0800

    Added PingAllHosts test
    Test seems to work, but exposed a bug in orc.

--- tests-legacy-l3/l3.py
+++ tests-legacy-l3/l3.py
@@ -60,7 +60,7 @@ class BasicL3Test(base_tests.DataPlaneOnly):
         p2 = scapy.Ether(str(recv))
         if p1.type != p2.type:
             return False    # not same ethertype
-        #pdb.set_trace()
+        pdb.set_trace()
         if p1.type == 0x0800:   # if IP, remove ID, chksum
             ip1 = p1.payload
             ip2 = p2.payload
@@ -133,7 +133,7 @@ class BasicL3Test(base_tests.DataPlaneOnly):
                 ports.append(port)
         return ports
 
-class PingCrossInterfacesL3(BasicL3Test):
+class PingEgressInterfacesL3(BasicL3Test):
     """ For each interface i
             verify we can ping each interface j"""
     def runTest(self):
@@ -185,10 +185,72 @@ class PingCrossInterfacesL3(BasicL3Test):
                 if rcv_pkt is not None:
                     pong_found = self.pkt_smart_cmp(expected=pong, recv=scapy.Ether(rcv_pkt))
 
+class PingAllHosts(BasicL3Test):
+    """ For each interface i
+            for each interface j
+        fake a host A off of interface i
+        fake a host B off of interface j
+            send a ping from A to B
+            verify that B receives the ping
+            don't reply
+    """
+    def runTest(self):
+        ports = self.getPrunedPorts()
+        if len(ports) < 2 :
+            self.fail("Need at least two ports to run this test")
+        for src_port in ports:
+            for dst_port in ports:
+                if src_port == dst_port:
+                    continue
+            ingress_gateway_ip = self.Interfaces[src_port]
+            egress_gateway_ip = self.Interfaces[dst_port]
+            dst_ip = self.same_slash24_subnet(self.Interfaces[dst_port])
+            src_ip = self.same_slash24_subnet(self.Interfaces[src_port])
+            src_mac = "00:11:22:33:44:%2d" % src_port
+            dst_mac = "00:11:22:33:44:%2d" % dst_port
+            # first arp for the gateway interface
+            ingress_gateway_mac = self.arpForMac(
+                            src_mac=src_mac,
+                            src_ip=src_ip,
+                            dst_ip=ingress_gateway_ip,
+                            port=src_port)
+            egress_gateway_mac = self.arpForMac(
+                            src_mac=dst_mac,
+                            src_ip=dst_ip,
+                            dst_ip=egress_gateway_ip,
+                            port=dst_port)
+            ping = testutils.simple_icmp_packet(        # request
+                                    ip_dst = dst_ip,  
+                                    ip_src = src_ip,
+                                    eth_dst = ingress_gateway_mac,
+                                    eth_src = src_mac
+                                    )
+            recv_ping = testutils.simple_icmp_packet(        # reply
+                                    ip_dst = src_ip,   
+                                    ip_src = dst_ip,
+                                    eth_dst = dst_mac,
+                                    eth_src = egress_gateway_mac,
+                                    ip_ttl = 63
+                                    )
+            logging.info("Sending ping to %s out port %d" % (dst_ip, src_port))
+            logging.debug("Data: " + str(ping).encode('hex'))
+            logging.debug("Expecting: " + str(ping).encode('hex'))
+            #scapy.hexdump(pong)
+            self.dataplane.send(src_port, str(ping))
+            start_time = time.time()
+            pong_found = False
+            while not pong_found:
+                timeout = (start_time + 3) - time.time()
+                if timeout <= 0:
+                    logging.error("Timeout on PING reply from port %d to port %d" % (src_port,dst_port))
+                    self.fail("Timeout on PING from port %d to port %d" % (src_port, dst_port))
+                (rcv_port, rcv_pkt, t) = self.dataplane.poll(port_number = dst_port, timeout=timeout)
+                if rcv_pkt is not None:
+                    pong_found = self.pkt_smart_cmp(expected=recv_ping, recv=scapy.Ether(rcv_pkt))
                 
         
 
-class PingInterfacesL3(BasicL3Test):
+class PingIngressInterfacesL3(BasicL3Test):
     """ For each interface, try to ping it """
     def runTest(self):
         for port in self.getPrunedPorts() : 
